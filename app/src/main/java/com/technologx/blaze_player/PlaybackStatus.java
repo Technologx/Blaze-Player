290243bb7bbac120c30149f4a15c2914df14cb38
Kevin N <support@technologx.com>
2017-01-17 03:42:20 -0600

    Initial Release

---
+++ app/src/main/java/com/technologx/blaze_player/PlaybackStatus.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Technologx
+ * Visit & signup on https://technologx.com
+ * Do not remove this removing this avoids the license
+ */
+
+package com.technologx.blaze.player;
+
+
+public enum PlaybackStatus {
+    PLAYING,
+    PAUSED
+}
